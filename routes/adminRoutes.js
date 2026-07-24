2f6bc8b4bd3354021587a75856006ab5586d7c39
Ayesha <Ayesha4211531@gmail.com>
2026-07-24 08:49:14 -0700

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth");
-const { getPendingWholesalers, updateBusinessStatus } = require("../controllers/adminController");
+const {
+ getPendingWholesalers, 
+ updateBusinessStatus,
+ getApprovedWholesalers,
+
+} = require("../controllers/adminController");
 
 router.get("/pending-wholesalers", authMiddleware, getPendingWholesalers);
 router.post("/update-status", authMiddleware, updateBusinessStatus);
+router.get("/wholesalers", authMiddleware, getApprovedWholesalers);
 
 module.exports = router;
