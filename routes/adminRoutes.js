8959bba1980833ace1316bd47760f39542825306
Ayesha <Ayesha4211531@gmail.com>
2026-07-25 10:33:29 -0700

    Working on adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,11 +5,15 @@ const {
  getPendingWholesalers, 
  updateBusinessStatus,
  getApprovedWholesalers,
-
+ getBuyers,
+ getAdminOrders,
 } = require("../controllers/adminController");
 
 router.get("/pending-wholesalers", authMiddleware, getPendingWholesalers);
 router.post("/update-status", authMiddleware, updateBusinessStatus);
 router.get("/wholesalers", authMiddleware, getApprovedWholesalers);
+router.get("/buyers", authMiddleware, getBuyers);
+router.get("/orders", authMiddleware, getAdminOrders);
+
 
 module.exports = router;
