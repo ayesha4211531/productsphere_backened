d0f63edeae157a2376d2478c94416ae46402b08f
Ayesha <Ayesha4211531@gmail.com>
2026-07-26 08:32:56 -0700

    Working on adminRoutes.js in all,deleteproductes,updateproductstatus

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,6 +7,9 @@ const {
  getApprovedWholesalers,
  getBuyers,
  getAdminOrders,
+ getAllProducts,
+ deleteProduct,
+ updateProductStatus
 } = require("../controllers/adminController");
 
 router.get("/pending-wholesalers", authMiddleware, getPendingWholesalers);
@@ -14,6 +17,9 @@ router.post("/update-status", authMiddleware, updateBusinessStatus);
 router.get("/wholesalers", authMiddleware, getApprovedWholesalers);
 router.get("/buyers", authMiddleware, getBuyers);
 router.get("/orders", authMiddleware, getAdminOrders);
+router.get("/products", authMiddleware, getAllProducts);
+router.delete("/products/:id", authMiddleware, deleteProduct);
+router.post("/products/status", authMiddleware, updateProductStatus);
 
 
 module.exports = router;
